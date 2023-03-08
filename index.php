635541b06cb7c914a0d2e5fe7104f140a2058ab2
sucipitaloka <absyxj@gmail.com>
2023-03-08 16:19:56 +0700

    update index

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 ?>
 <html lang="en">
 <head>
-	<title>Login | MDCAFE</title>
+	<title>Login | MDLOUNGE</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
